f6d4ec32f1758fcbf577e23b37e72eb0397f5a4f
mishagansvind <54382490+mishagansvind@users.noreply.github.com>
2019-09-13 19:51:50 -0500

    numberOfDigits wasn't declared before it was found in a loop

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -8,7 +8,7 @@
  * integer n whose square can be split into two positive integers
  * whose sum is equal to the original number.  For the purposes of
  * this method, we restrict our consideration to base-10 representations.</p>
- * 
+ *
  * For example, 45 is a Kaprekar number, because
  *   45<sup>2</sup> = 2025
  * and
@@ -35,7 +35,7 @@ int isKaprekar(int n) {
   int numDigits = (int) log10(n) + 1;
   long modulus = 0;
   long first, second;
-
+  int numberOfDigits;
   //for each possible "split" of the square...
   for(i=1; i<=numberOfDigits; i++) {
     //increase the modulus by a factor of 10
