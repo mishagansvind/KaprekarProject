078ca22eef0a2b99e1476bcdfe8a20e34bc80cb4
mishagansvind <54382490+mishagansvind@users.noreply.github.com>
2019-09-13 19:55:02 -0500

    misspelled modulus when calling it

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -36,10 +36,11 @@ int isKaprekar(int n) {
   long modulus = 0;
   long first, second;
   int numberOfDigits;
+
   //for each possible "split" of the square...
   for(i=1; i<=numberOfDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
